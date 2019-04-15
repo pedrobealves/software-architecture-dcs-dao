9e4be18c753c2a82ddd8a11f3d97c427912eecf0
PedroTreck <pedrobealves@gmail.com>
2019-04-14 23:12:56 -0300

    Feat: added dao components

--- src/main/java/br/edu/utfpr/service/CountryService.java
+++ src/main/java/br/edu/utfpr/service/CountryService.java
@@ -1,4 +1,18 @@
 package br.edu.utfpr.service;
 
+import br.edu.utfpr.dto.CountryDTO;
+
+import java.util.List;
+
 public class CountryService {
+
+
+    public void create(CountryDTO country){
+
+
+    }
+
+    public List<CountryDTO> list() {
+        return null;
+    }
 }
